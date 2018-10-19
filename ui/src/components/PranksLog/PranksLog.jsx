26c052da3f231efc744bdc11cfbde56365c1aabd
SebMaz <seb.maz93@icloud.com>
2018-10-20 02:05:21 +0400

    Prod Build test

--- ui/src/components/PranksLog/PranksLog.jsx
+++ ui/src/components/PranksLog/PranksLog.jsx
@@ -9,6 +9,7 @@ import PrankScore from './PrankScore';
 // Styles
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
+import Button from 'react-bootstrap/lib/Button';
 
 const socket = io('http://localhost:8081');
 
@@ -19,6 +20,7 @@ class PranksLog extends Component {
 
   componentDidMount() {
     socket.connect();
+    console.log('Socket Connected', 'Will auto DC in 30sec');
     socket.on('new-prank-activity', data => {
       if (data) {
         this.setState({items: [...this.state.items, data]});
@@ -28,13 +30,18 @@ class PranksLog extends Component {
     });
     setTimeout(() => {
       socket.disconnect();
-    }, 4000);
+      console.log('Socket AUTO Disconnected');
+    }, 30000);
   }
+  dc = () => {
+    socket.disconnect();
+    console.log('Socket Disconnected !');
+  };
 
   render() {
     const {items} = this.state;
     return (
-      <Col lg='10' md="9">
+      <Col lg="10" md="9">
         <Row>
           <Col md="8">
             <Row className="px-3">
@@ -43,6 +50,14 @@ class PranksLog extends Component {
             <PranksLogList items={items} />
           </Col>
           <PrankScore score={items.length} />
+        </Row>
+        <Row>
+          <Col sm="6">
+            <h5>after loading too much items press the RED button</h5>
+            <Button variant="danger" onClick={this.dc}>
+              Disconnect Socket
+            </Button>
+          </Col>
         </Row>
       </Col>
     );
